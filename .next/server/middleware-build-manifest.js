9c66e2b14b6adea9a85dd6db0594d9179dc71a5a
Atharve Dahima <atharveeee@gmail.com>
2026-08-14 14:29:55 +0530

    Add GitHub Pages workflow and configure Next.js static export

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -7,15 +7,15 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/0cz1d0mv5g_q7.js"
   ],
   "lowPriorityFiles": [
-    "static/HjH6zPYqlZu95KjeeEHt2/_buildManifest.js",
-    "static/HjH6zPYqlZu95KjeeEHt2/_ssgManifest.js",
-    "static/HjH6zPYqlZu95KjeeEHt2/_clientMiddlewareManifest.js"
+    "static/11FJMc5MQUzcTEcMQSToq/_buildManifest.js",
+    "static/11FJMc5MQUzcTEcMQSToq/_ssgManifest.js",
+    "static/11FJMc5MQUzcTEcMQSToq/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/3l04zcqx63h3y.js",
-    "static/chunks/2y4wans9ulj_u.js",
+    "static/chunks/10jjie-v-1l96.js",
     "static/chunks/08ttfj81-47mu.js",
-    "static/chunks/turbopack-1m14ias-r6ul9.js"
+    "static/chunks/turbopack-3ro06getfgqcx.js"
   ],
   "rootMainFilesTree": {},
   "pagesChunkGroupBootstrapParams": {},
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* standard Next.js 16 App Router configuration */
+  output: "export",
+  basePath: "/qashu",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
